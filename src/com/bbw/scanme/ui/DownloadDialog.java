92fb71b83dfd6ba61c020b056f7853cf3b4e5b21
supper008 <supper008@gmail.com>
2013-06-25 10:46:51 +0800

    d

---
+++ src/com/bbw/scanme/ui/DownloadDialog.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.bbw.scanme.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author BBW
+ *	É¨ÃèºóµÄÏÂÔØ¿ò
+ */
+public class DownloadDialog extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO Auto-generated method stub
+
+	}
+
+}
