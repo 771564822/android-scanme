b35c2fa4600584d6aedf8ac68acea47f28c63435
supper008 <supper008@gmail.com>
2013-06-25 11:13:12 +0800

    d

---
+++ src/com/bbw/scanme/ui/dialog/DownloadDialog.java
@@ -0,0 +1,14 @@
+package com.bbw.scanme.ui.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class DownloadDialog extends Dialog {
+
+	public DownloadDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
